be3e288051e9a7d7ecd9b548643ec90d76e52446
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2024-10-26 22:49:58 +0400

    Operations

---
+++ src/main/java/as/HomeTask2.java
@@ -0,0 +1,33 @@
+package as;
+
+import java.util.Scanner;
+
+public class HomeTask2 {
+
+    public static void main(String[] args) {
+        int a =5;
+        // b, a ile eyni olacaq cunki birinci a oldugu kimi qalir sonra 1 vahid artir.
+        int b= a++;
+        // c ise a artiq 6 olmusdu ++a olduguna gore bir vahid artacaq ve 7 olacaq.
+        int c= ++a;
+        System.out.printf("b is equal to:%d\n",b);
+        System.out.println("c is equal to:"+c);
+
+        int d=78;
+        // d+=4 --> d=d+4 d-ni 4 vahid artiririq.
+        d+=4;
+        int e=23;
+        // e-=6 --> e=e-6 e-ni 6 vahid azaldiriq.
+        e-=6;
+        System.out.println("d is equal to:"+d);
+        System.out.println("e is equal to:"+e);
+
+        int f =14;
+        // f-- --> g, ile eyni olacaq daha sonra f bir vahid azalacaq.
+        int g= f--;
+        // h ise f artiq 13 olmusdu --a olduguna gore bir vahid azalacaq ve 12 olacaq.
+        int h= --f;
+        System.out.printf("g is equal to:%d\n",g);
+        System.out.print("h is equal to:"+h);
+    }
+}
--- src/main/java/as/NumberApp.java
+++ src/main/java/as/NumberApp.java
@@ -3,11 +3,13 @@ package as;
 import java.util.Scanner;
 
 public class NumberApp {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = scan.nextInt();
-        System.out.println((num % 2 ==1 ) ? "tek" : "cut ");
+        System.out.println((num % 2 ==1 ) ? "tek" : "cut");
 
     }
 }
