e69d6ce09f5574591c8e2492dd0920de37ba7ff9
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2024-10-26 17:53:02 +0400

    Addition

---
+++ src/main/java/as/Addition.java
@@ -0,0 +1,17 @@
+package as;
+
+public class Addition {
+    public static void main(String[] args) {
+        int num1=15;
+        int num2 =7;
+        System.out.println(num1 +" + "+ num2+ "= "+ num1+num2);
+        //System.out.println(num1 +" - "+ num2+ "= "+ num1-num2);
+        System.out.println(num1 +" * "+ num2+ "= "+ num1*num2);
+        System.out.println(num1 +" / "+ num2+ "= "+ num1/num2);
+        System.out.println(num1 +" % "+ num2+ "= "+ num1%num2);
+
+    }
+
+
+
+}
