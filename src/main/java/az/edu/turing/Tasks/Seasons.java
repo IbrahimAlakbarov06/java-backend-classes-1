081c664b33d741a4dc97c3f0c8f98a259cf9bf91
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2024-11-03 13:52:06 +0400

    TASK

--- src/main/java/az/edu/turing/Tasks/Seasons.java
+++ src/main/java/az/edu/turing/Tasks/Seasons.java
@@ -8,6 +8,6 @@ public class Seasons {
         System.out.println("Enter number: ");
         int num = scan.nextInt();
 
-        System.out.println(num == 1 ? "It's Janruary and Winter" : num == 2 ? "It's February and Winter" : num == 3 ? "It's Janruary and Winter" : num == 3 ? "It's Janruary and Winter" : num == 4 ? "It's Janruary and Spring" : num == 5 ? "It's May and Spring" : num == 6 ? "It's June and Summer" : num == 7 ? "It's July and Summer" : num == 8 ? "It's August and Summer" : num == 9 ? "It's September and Autumn" : num == 10 ? "It's October and Autumn" : num == 11 ? "It's November and Autumn" : num == 12 ? "It's December and Winter" : "Wrong");
+        System.out.println(num == 1 ? "It's January and Winter" : num == 2 ? "It's February and Winter" : num == 3 ? "It's March and Spring" : num == 4 ? "It's April and Spring" : num == 5 ? "It's May and Spring" : num == 6 ? "It's June and Summer" : num == 7 ? "It's July and Summer" : num == 8 ? "It's August and Summer" : num == 9 ? "It's September and Autumn" : num == 10 ? "It's October and Autumn" : num == 11 ? "It's November and Autumn" : num == 12 ? "It's December and Winter" : "Wrong");
     }
 }
