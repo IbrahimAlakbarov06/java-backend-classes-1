4336934de86aa68c0ee587d5b955b7ca81639133
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2024-12-01 13:42:40 +0400

    Employee

--- src/main/java/az/edu/turing/module2/lesson11/Taskk/Employee.java
+++ src/main/java/az/edu/turing/module2/lesson11/Taskk/Employee.java
@@ -1,12 +1,18 @@
 package az.edu.turing.module2.lesson11.Taskk;
 
-public abstract class Employee {
+public class Employee {
     private String name;
     private double salary;
 
     public Employee(String name, double salary){
         this.name=name;
         this.salary=salary;
+        if(isNull() == false){
+
+        }
+    }
+    public Employee(){
+
     }
 
     public String getName() {
@@ -23,6 +29,14 @@ public abstract class Employee {
 
     public void setSalary(double salary) {
         this.salary = salary;
+    }
+    public boolean isNull(){
+        boolean flag = true;
+        if(name == null){
+            System.out.println("You have to input name: ");
+            flag = false;
+        }
+        return flag;
     }
 
     public void displayInfo(){
--- src/main/java/az/edu/turing/module2/lesson11/Taskk/EmployeeApp.java
+++ src/main/java/az/edu/turing/module2/lesson11/Taskk/EmployeeApp.java
@@ -1,14 +1,22 @@
 package az.edu.turing.module2.lesson11.Taskk;
-
+import java.util.Scanner;
 public class EmployeeApp {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Employee[] employee =new Employee[2];
-        employee[0]=new Manager("Ali",2000,3);
+        employee[0]=new Manager(null, 2000,3);
         employee[1]= new Developer("Ibis", 5000, "Java");
+
 
        // employee[0].calculateBonus(10);
         for (int i =0; i< employee.length; i++){
             employee[i].calculateBonus(10);
+            while(employee[i].isNull()){
+
+            }
+
+
+
         }
     }
 
